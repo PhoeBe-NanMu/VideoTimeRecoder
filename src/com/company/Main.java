4c879e2c8cd6db507f2f559618382935a8465803
LeiYang <2443638666@qq.com>
2018-11-27 20:51:22 +0800

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,26 +8,12 @@ public class Main {
     public static void main(String[] args) {
 
 
-        String fileFolderPath = "/Volumes/Learning Materials/2019【朱伟】英语/基础班/05【朱伟】【新版恋练有词】/1-10单元";
-        String fileFolderPath0 = "/Volumes/Learning Materials/2019【朱伟】英语/基础班/05【朱伟】【新版恋练有词】/11--20单元";
-        String fileFolderPath00 = "/Volumes/Learning Materials/2019【朱伟】英语/基础班/05【朱伟】【新版恋练有词】/21~30单元";
-        String fileFolderPath1 = "/Users/leiyang/Downloads/temp";
-        String fileFolderPath2 = "/Volumes/Learning Materials/2019【汤家凤面授】专项/05 暑期面授强化班/高数替换版【推荐】";
-        String fileFolderPath3 = "/Volumes/Learning Materials/2019【汤家凤面授】专项/05 暑期面授强化班/线代";
-        String fileFolderPath4 = "/Volumes/Learning Materials/2019【肖秀荣】政治/01.肖秀荣2019考研政治命题人知识点精讲精练";
-        String fileFolderPath5 = "/Volumes/Learning Materials/2019考研【计算机408】【30元】/2019文都计算机/04.2019考研计算机强化课程/03.操作系统【完】";
-        String fileFolderPath6 = "/Volumes/Learning Materials/2019考研【计算机408】【30元】/2019文都计算机/04.2019考研计算机强化课程/02.计算机网络【完】";
-        String fileFolderPath7 = "/Volumes/Documents/数学目录/05 暑期面授强化班/线代";
-        String fileFolderPath8 = "/Volumes/Documents/数学目录/05 暑期面授强化班/概率论与数理统计强化";
-        String fileFolderPath9 = "/Volumes/Materials/政治目录/05-2019徐涛专项/2019考研政治徐涛强化班【2019考研 必加微信：qcp2019 赠课】";
-        String fileFolderPath10 = "/Volumes/Materials/百度网盘/计算机网络【完】";
-        String fileFolderPath11 = "/Volumes/Materials/百度网盘/操作系统【完】";
-        String fileFolderPath12 = "/Volumes/Materials/百度网盘/计算机网络【完】";
+        String fileFolderPath = "/Volumes/Materials/百度网盘/计算机网络【完】";
 
 
 
         VideoFolderTimeCount videoFolderTimeCount = new VideoFolderTimeCount();
-        videoFolderTimeCount.getVideoDuration(fileFolderPath12,1);
+        videoFolderTimeCount.getVideoDuration(fileFolderPath,1);
     }
 }
 
