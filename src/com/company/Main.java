c08b90591604e6d5fa17ceefdefaa55266ca2181
leiyang2022 <2443638666@qq.com>
2022-09-30 16:49:25 +0800

    添加ffmpeg位置说明,测试Github

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,10 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         String fileFolderPath = "/home/nanmu/百度网盘/Python3入门人工智能 掌握机器学习+深度学习（完整版） - 448元";
-
-
 
         VideoFolderTimeCount videoFolderTimeCount = new VideoFolderTimeCount();
         videoFolderTimeCount.getVideoDuration(fileFolderPath,1);
